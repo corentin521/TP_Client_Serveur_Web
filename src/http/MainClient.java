384915f08bedf45614cde7217bee43096683cd0a
Hugo Chomarat <hugo.chomarat@etu.univ-lyon1.fr>
2017-05-24 08:36:27 +0200

    start

---
+++ src/http/MainClient.java
@@ -0,0 +1,16 @@
+package http;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MainClient {
+    
+    
+    public static void main(String[] args) throws IOException
+    {
+        String requete = " ";
+        Client c = new Client(requete);
+    }
+    
+}
