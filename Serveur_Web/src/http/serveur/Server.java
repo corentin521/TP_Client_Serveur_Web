6ae3e86d43d328711ba3a76d720e375c6d76ae67
Hugo Chomarat <hugo.chomarat@etu.univ-lyon1.fr>
2017-06-08 13:22:31 +0200

    test

--- Serveur_Web/src/http/serveur/Server.java
+++ Serveur_Web/src/http/serveur/Server.java
@@ -1,6 +1,10 @@
 package http.serveur;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
@@ -29,28 +33,60 @@ public class Server implements Runnable {
         System.out.println("Thread running on Server");
         this.isRunning = true;
         
+        
         while(this.isRunning){
             try{
                 try (Socket clientSocket = serverSocket.accept()) {
                 
+                    DataInputStream inFromClient = null;
+                    inFromClient = new DataInputStream(clientSocket.getInputStream());
+
+                    DataOutputStream outToClient = null;
+                    outToClient = new DataOutputStream(clientSocket.getOutputStream());
+                    
                     InputStreamReader isr =  new InputStreamReader(clientSocket.getInputStream());
                     BufferedReader br = new BufferedReader(isr);
                     String line = br.readLine();
                     String [] parts = line.split(" ");
 
+                    System.out.println(line);
                     // Affichage de la requête
                     while (!line.isEmpty()) {
                         line = br.readLine();
-                        System.out.println(line);
+                        //System.out.println(line);
                     }
 
                     // Réponse du serveur
-                    if(parts[0].equals("GET")){
+                    if(parts[0].equals("GET"))
+                    {
                         System.out.println("Requête GET");
+                        
+                        File f = new File("testClient.txt");
 
-                        String message = "Réponse du serveur";
-                        String httpHead = "HTTP/1.1 200 OK\r\n\r\n" + message;
-                        clientSocket.getOutputStream().write(httpHead.getBytes("UTF-8"));
+                        //File f = new File(parts[1].replace('/', '\\'));
+                        
+                        System.out.println(f);
+                        if (f!=null)
+                        {
+                            //FileOutputStream file = new FileOutputStream(f);
+                            String message = "good";
+                            String httpHead = "HTTP/1.1 200 OK\r\n\r\n" + message;
+                            clientSocket.getOutputStream().write(httpHead.getBytes("UTF-8"));
+                            
+                            
+                            
+                            outToClient.writeBytes(getResp + "\n\n");
+                            outToClient.flush();
+
+                            outToClient.write(bufferEnvoi);
+                            outToClient.close();
+                        }
+                        else
+                        {
+                            String message = "Réponse du serveur : Le fichier "+parts[1]+" n'existe pas";
+                            String httpHead = "HTTP/1.1 200 OK\r\n\r\n" + message;
+                            clientSocket.getOutputStream().write(httpHead.getBytes("UTF-8"));
+                        }
                     }
                 }
 
